755b4e0f200381b45e674047f7a782626be75fac
Brian <bbg.visualjournalist@gmail.com>
2018-05-07 15:53:12 -0400

    Experimenting with opening a popup window for FB share.

--- js/script-story.js
+++ js/script-story.js
@@ -120,6 +120,11 @@ $(document).ready(function(){
 		window.open(url, '', 'left=0,top=0,width=550,height=450,personalbar=0,toolbar=0,scrollbars=0,resizable=0');
 		return false;
 	})
+	$('#shareFacebook').click(function(){
+		var url = $(this).attr("href");
+		window.open(url, '', 'left=0,top=0,width=550,height=450,personalbar=0,toolbar=0,scrollbars=0,resizable=0');
+		return false;
+	})
 
 
 
